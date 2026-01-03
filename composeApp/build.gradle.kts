4825bf1cdbf7bc4d43003882ce6ccbcbdfcddc82
Cristofer Canosa Dominguez <cristofer_cd@outlook.es>
2026-01-03 20:04:29 +0100

    v0.0.4

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -60,7 +60,7 @@ compose.desktop {
             //suggestRuntimeModules task
             modules("java.instrument", "java.management", "java.sql", "java.naming", "jdk.unsupported")
             packageName = "es.cristcd.taskcompanion"
-            packageVersion = "0.0.3"
+            packageVersion = "0.0.4"
 
             windows {
                 dirChooser = true
