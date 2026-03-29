61219f39ddcd95a7cb19fb6d246b9144dbb12a74
Cristofer Canosa Dominguez <cristofer_cd@outlook.es>
2026-03-29 14:41:54 +0200

    Prevent db from getting deleted on update

--- composeApp/src/commonMain/kotlin/es/cristcd/taskcompanion/persistence/PersistenceManager.kt
+++ composeApp/src/commonMain/kotlin/es/cristcd/taskcompanion/persistence/PersistenceManager.kt
@@ -15,11 +15,18 @@ import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
 import org.jetbrains.exposed.v1.jdbc.transactions.TransactionManager
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import java.io.File
 import java.sql.Connection
 
 object PersistenceManager {
     fun init() {
-        val db = Database.connect("jdbc:sqlite:data.db", "org.sqlite.JDBC")
+        val dbDir = if (System.getenv("app.env") == "dev") {
+            "data.db"
+        } else {
+            val appDataDir = getAppDataDir().apply { mkdirs() }
+            "$appDataDir/data.db"
+        }
+        val db = Database.connect("jdbc:sqlite:$dbDir", "org.sqlite.JDBC")
         TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE // or Connection.TRANSACTION_READ_UNCOMMITTED
         TransactionManager.defaultDatabase = db
 
@@ -39,4 +46,14 @@ object PersistenceManager {
             )
         }
     }
+
+    private fun getAppDataDir(): File {
+        val os = System.getProperty("os.name").lowercase()
+        return when {
+            os.contains("win") -> File(System.getenv("LOCALAPPDATA"), ".TaskCompanion")
+            os.contains("mac") -> File(System.getProperty("user.home"), "Library/Application Support/.TaskCompanion")
+            else -> File(System.getProperty("user.home"), ".TaskCompanion")
+        }
+    }
+
 }
