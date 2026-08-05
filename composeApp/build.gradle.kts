1ea469534398350d98a97ee69cb2c3c20a785393
Cristofer Canosa Dominguez <cristofer_cd@outlook.es>
2026-08-05 21:27:22 +0200

    v0.4.7

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -69,7 +69,7 @@ compose.desktop {
             //suggestRuntimeModules task
             modules("java.instrument", "java.management", "java.sql", "java.naming", "jdk.unsupported")
             packageName = "es.cristcd.taskcompanion"
-            packageVersion = "0.4.6"
+            packageVersion = "0.4.7"
 
             windows {
                 shortcut = true
@@ -79,7 +79,7 @@ compose.desktop {
                 upgradeUuid = "7f4ce14d-1896-4550-87ed-29b333793066"
             }
             macOS {
-                packageVersion = "1.4.6"
+                packageVersion = "1.4.7"
             }
         }
     }
